d1d1c36a5296184b1fa902a8014239080998223a
MateusNunesAraujo <mnunexaraujo@gmail.com>
2025-01-25 10:20:19 -0500

    Actualizando portafolio

--- js/main.js
+++ js/main.js
@@ -12,29 +12,71 @@ let proyectos = document.querySelector('.cont-proyecto')
 let cerrarBtn = document.querySelector('.ico-cerrar')
 let contactameBtn = document.querySelector('#contactame')
 let contactameReducidoBtn = document.querySelector('#contactame-reducido')
+let btnLeft = document.querySelector(".btn-left")
+let btnRight = document.querySelector(".btn-right")
+let slider = document.querySelector("#slider")
+let sliderSection = document.querySelectorAll(".slider-section")
+let operacion = 0
+let counter = 0
+let widthImg = 100 / sliderSection.length
 
-document.addEventListener('DOMContentLoaded',()=>{
+setInterval(()=>{
+    moveToRight()
+},3500)
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+function moveToRight() {
+    if (counter >= sliderSection.length - 1) {
+        counter = 0
+        operacion = 0
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "all ease .6s"
+        return
+    }
+    counter++
+    operacion = operacion + widthImg
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s"
+}
+
+
+function moveToLeft() {
+    counter--
+    if (counter < 0) {
+        counter = sliderSection.length - 1
+        operacion = widthImg * (sliderSection.length - 1)
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "all ease .6s"
+        return
+    } 
+        operacion = operacion - widthImg
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "all ease .6s"
+}
+
+document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('.primera-foto').classList.add('primera-foto-activacion')
 
 })
 
-contactameReducidoBtn.addEventListener('click',()=>{
-    if(!document.querySelector('.redes-sociales').classList.contains('redes-sociales-activado')){
+contactameReducidoBtn.addEventListener('click', () => {
+    if (!document.querySelector('.redes-sociales').classList.contains('redes-sociales-activado')) {
         contactameReducidoBtn.textContent = '\u2715 Cerrar lista'
         document.querySelector('.redes-sociales').classList.add('redes-sociales-activado')
-    }else{
+    } else {
         contactameReducidoBtn.textContent = 'Contactame'
         document.querySelector('.redes-sociales').classList.remove('redes-sociales-activado')
     }
-  
+
 })
 
-contactameBtn.addEventListener('click',()=>{
-    if(!document.querySelector('.redes-sociales').classList.contains('redes-sociales-activado')){
+contactameBtn.addEventListener('click', () => {
+    if (!document.querySelector('.redes-sociales').classList.contains('redes-sociales-activado')) {
         contactameBtn.textContent = '\u2715 Cerrar lista'
         document.querySelector('.redes-sociales').classList.add('redes-sociales-activado')
-    }else{
+    } else {
         contactameBtn.textContent = 'Contactame'
         document.querySelector('.redes-sociales').classList.remove('redes-sociales-activado')
     }
@@ -45,38 +87,38 @@ contactameBtn.addEventListener('click',()=>{
 })
  */
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let ubicacion = proyectos.getBoundingClientRect().top;
     console.log(ubicacion)
-    if(ubicacion < 250){
+    if (ubicacion < 250) {
         proyectos.classList.add('cont-proyecto-activado')
     }
-   
+
 })
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let ubicacion = educacion.getBoundingClientRect().top;
 
-    if(ubicacion < 250){
+    if (ubicacion < 250) {
         educacion.classList.add('cont-educacion-activado')
     }
-   
+
 })
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let ubicacion = foto.getBoundingClientRect().top;
-    if(ubicacion < 460){
-    foto.classList.add('foto-activado')
+    if (ubicacion < 460) {
+        foto.classList.add('foto-activado')
     }
 })
 
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let ubicacion = pagina.getBoundingClientRect().top;
-    if(ubicacion < 0){
-    upBtn.classList.add('up-activado')
-    }else{
-    upBtn.classList.remove('up-activado')
+    if (ubicacion < 0) {
+        upBtn.classList.add('up-activado')
+    } else {
+        upBtn.classList.remove('up-activado')
     }
 })
 
@@ -84,53 +126,53 @@ window.addEventListener('scroll',()=>{
 /* let habilidades = document.getElementById('habilidades').addEventListener('') */
 opcionesNavegacion.forEach(element => {
 
-    element.addEventListener('mouseover',(e)=>{
+    element.addEventListener('mouseover', (e) => {
         console.log(e.target.nextElementSibling)
-    e.target.nextElementSibling.style.width = '100%'
+        e.target.nextElementSibling.style.width = '100%'
     })
 
 })
 
 opcionesNavegacion.forEach(element => {
 
-    element.addEventListener('mouseout',(e)=>{
+    element.addEventListener('mouseout', (e) => {
         console.log(e.target.nextElementSibling)
-    e.target.nextElementSibling.style.width = '0'
+        e.target.nextElementSibling.style.width = '0'
     })
 
 })
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let ubicacion = habilidades.getBoundingClientRect().bottom;
-    if(ubicacion < 1200){
+    if (ubicacion < 1200) {
         barraHabilidades.forEach(element => {
             let porcentaje = element.children[1].textContent
             element.children[2].querySelector('.barra-interna').style.width = porcentaje
         });
-        }else{
-            barraHabilidades.forEach(element => {
-                element.children[2].querySelector('.barra-interna').style.width = '0px'
-            });
-        }
-    
+    } else {
+        barraHabilidades.forEach(element => {
+            element.children[2].querySelector('.barra-interna').style.width = '0px'
+        });
+    }
+
 })
 /* barraHabilidades.forEach(element => {
     let porcentaje = element.children[1].textContent
     element.children[2].querySelector('.barra-interna').style.width = porcentaje
 }); */
 
-flechaAdelanteBtn.addEventListener('click',()=>{
-document.querySelector('.cont-habilidades').classList.add('cont-habilidades-ocultas')
-document.querySelector('.cont-frameworks').classList.add('cont-frameworks-activas')
+flechaAdelanteBtn.addEventListener('click', () => {
+    document.querySelector('.cont-habilidades').classList.add('cont-habilidades-ocultas')
+    document.querySelector('.cont-frameworks').classList.add('cont-frameworks-activas')
 
 })
 
-flechaAtrasBtn.forEach(element =>{
-    element.addEventListener('click',()=>{
+flechaAtrasBtn.forEach(element => {
+    element.addEventListener('click', () => {
         document.querySelector('.cont-habilidades').classList.remove('cont-habilidades-ocultas')
         document.querySelector('.cont-frameworks').classList.remove('cont-frameworks-activas')
-        
-        })
+
+    })
 })
 
 btnMenu.addEventListener('click', (e) => {
@@ -155,4 +197,6 @@ btnMenu.addEventListener('click', (e) => {
         document.querySelector('.opciones ul').classList.remove('opciones-activado')
     }
 })
+
+
 
